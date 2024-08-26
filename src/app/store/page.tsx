bfa3584ed380ea929ca306474d19f5e9429f99a6
Ulvi.Nasib <49786576+ulvi123@users.noreply.github.com>
2024-08-26 16:28:50 +0300

    features section content changed

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -7,8 +7,8 @@ import ProductList from "@/components/store/ProductList"
 export default function Store() {
   return (
     <div className="pt-32">
-      <h1 className="text-3xl font-bold font-sans text-center" >Voxpal Merch store</h1>
-      <ProductList/>
+      <h1 className="text-7xl font-bold font-sans text-center pt-32" >Coming soon</h1>
+      {/* <ProductList/> */}
     </div>
   )
 }
